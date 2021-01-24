1bd2ad52c9b7dffc7afe68f710f6a006f5261a6b
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-01-25 01:23:01 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -4,9 +4,7 @@ def calculateStats(numbers):
   
   #If list is empty return NaN
   if( len(numbers) == 0 ):
-    average = math.nan
-    maximum = math.nan
-    minimum = math.nan
+    average = maximum = minimum = math.nan
   else:
     #Find average, Maximum, Minimum
     average  = sum(numbers) / len(numbers)
